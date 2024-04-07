df93f7efa82b69970fadaa42ea6defc4818bbedb
Deep <deeppatel223204@gmail.com>
2024-04-07 16:57:52 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,53 @@
+const connectDB = require("./db/connect");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const url = process.env.MONG_URI;
+
+const app = express();
+app.use(cors());
+
+const login = require("./routes/login");
+const port = 4000;
+
+app.use(express.json());
+app.use("/api", login);
+
+const start = async () => {
+  try {
+    await connectDB(url);
+    app.listen(port, () => {
+      console.log(`listening to port ${port} `);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
+
+/* const connectDB = require("./db/connect");
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// Define your routes and middleware here
+// For example:
+// app.use(express.json()); // Middleware for parsing JSON
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error("Error starting the server:", err.message);
+  }
+};
+
+start();
+ */
---
+++ server/controllers/login.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+
+const createUser = async (req, res) => {
+  try {
+    const exists = await User.findOne({ email: req.body.email });
+    if (exists) {
+      return res
+        .status(409)
+        .json({ message: "User is already present", exists });
+    } else {
+      const user = await User.create(req.body);
+      return res.status(201).json({ user });
+    }
+  } catch (err) {
+    console.error(err);
+    // Handle the error and send an appropriate response
+    return res
+      .status(500)
+      .json({ err: err.message || "Internal Server Error" });
+  }
+};
+
+const findUser = async (req, res) => {
+  try {
+    const { email, pwd: password } = req.params;
+    // const { password } = req.body;
+    console.log(password);
+    // console.log(req.body);
+    const user = await User.findOne({ email: email });
+    if (!user)
+      return res.status(400).json({ fetch: "un-successful, usernotFound" });
+    if (user.password === password)
+      return res.status(201).json({ fetch: "successful", user });
+    else
+      return res
+        .status(400)
+        .json({ fetch: "un-successful, please enter correct password" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ err });
+  }
+};
+module.exports = { createUser, findUser };
