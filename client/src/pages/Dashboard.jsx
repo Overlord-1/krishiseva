4f3caf166fa1e9637f6a64cd8a5ffa9f1821f42b
Overlord <ruchirkhare12@gmail.com>
2024-04-08 12:24:08 +0530

    Add useMemo hook to Dashboard component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import "../Dashboard.css";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
