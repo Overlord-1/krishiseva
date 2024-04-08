94435f3b2324ae4a876872eda90ca3915a30dbf9
Overlord <ruchirkhare12@gmail.com>
2024-04-08 11:38:11 +0530

    Add framer-motion library and update Dashboard component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Dashboard.css";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
-import io from 'socket.io-client';
+import {io} from 'socket.io-client';
+import {motion} from 'framer-motion';
+import chat from "../assets/chat.svg";
+import cropcycle from "../assets/cropcycle.svg";
+import leafrecog from "../assets/leafrecog.svg";
 
 const Dashboard = () => {
 
-  const socket = io('http://localhost:3000');
-  socket.on('connection', () => {
-    console.log('Connected to server');
-  });
+  // const socket = io('http://localhost:3000');
+  // socket.on('connection', () => {
+  //   console.log('Connected to server');
+  // });
+  const [pageState, setPageState] = useState(0);
+  const handleFooterClick = (e) => {
+    console.log(e.target.alt);
+    if (e.target.alt === "cropcycle") {
+      setPageState(0);
+    } else if (e.target.alt === "leafrecog") {
+      setPageState(1);
+    }
+    else if (e.target.alt === "chat") {
+      setPageState(2);
+    }
+  }
 
-  
   return (
     <>
       <Header />
-      <Navbar/>
+      {/* <Navbar/> */}
+      <div className="footer w-full bg-[#1e1f26] text-white flex px-5 justify-between fixed bottom-0  z-20 rounded-t-2xl">
+          <motion.div whileTap={{ scale: 0.4 }} className={`${pageState === 0 ? "bg-[#6cff7379]" : "bg-[#1e1f26]"} py-2 px-5 m-2 rounded-3xl`}>
+            <img
+              src={cropcycle}
+              alt="cropcycle"
+              className="w-[40px]"
+              onClick={(e) => handleFooterClick(e)}
+            />
+          </motion.div>
+          <motion.div whileTap={{ scale: 0.5 }} className={`${pageState === 1 ? "bg-[#feaeae]" : "bg-[#959fee]"} py-2 px-5 m-2 rounded-3xl`}>
+            <img
+              src={leafrecog}
+              alt="leafrecog"
+              className="w-[40px]"
+              onClick={(e) => handleFooterClick(e)}
+            />
+          </motion.div>
+          <motion.div whileTap={{ scale: 0.5 }} className={`${pageState === 2 ? "bg-[#feaeae]" : "bg-[#959fee]"} py-2 px-5 m-2 rounded-3xl`}>
+            <img
+              src={chat}
+              alt="chat"
+              className="w-[40px]"
+              onClick={(e) => handleFooterClick(e)}
+            />
+          </motion.div>
+        </div>
 
 
       
