0e6285f798866a93664ffe37cd76708d09b237e0
Deep <deeppatel223204@gmail.com>
2024-04-08 12:09:11 +0530

    socket changes

--- server/app.js
+++ server/app.js
@@ -2,25 +2,41 @@ require("dotenv").config();
 const connectDB = require("./db/connect");
 const express = require("express");
 const cors = require("cors");
-const app = express();
-// app.use(cors());
 const { Server } = require("socket.io");
 const { createServer } = require("http");
-const url = process.env.MONG_URI;
 
+const url = process.env.MONG_URI;
+const port = 4000;
+
+const app = express();
+app.use(cors()); // CORS middleware
+
+// Middleware for parsing JSON request bodies
+app.use(express.json());
+
+// Routes
+const login = require("./routes/login");
+app.use("/api", login);
+
+// Create HTTP server
 const server = createServer(app);
 
+// Initialize Socket.io with custom path
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: "*", // Update this to match your frontend URL
+    methods: ["GET", "POSsT"],
     credentials: true,
   },
-  path: "/socket.io", // Specify a custom path for Socket.io requests
+  // path: "/socket.io", // Specify a custom path for Socket.io requests
 });
 
+// Socket.io event handlers
 io.on("connection", (socket) => {
   console.log("User connected, ID:", socket.id);
+  socket.on("mes", (e) => {
+    console.log(e);
+  });
 
   socket.broadcast.emit("welcome", `${socket.id} joined the server`);
 
@@ -29,52 +45,16 @@ io.on("connection", (socket) => {
     io.to(room).emit("recieve-message", message); // Send the message to all clients in the room
   });
 
-  socket.on("join-room", (room) => {
-    console.log(`User ${socket.id} joined room ${room}`);
-    socket.join(room); // Join the room specified in the payload
-  });
-
   socket.on("disconnect", () => {
     console.log("User disconnected", socket.id);
   });
 });
 
-const login = require("./routes/login");
-const port = 4000;
-
-app.use(express.json());
-app.use("/api", login);
-
+// Start the server
 const start = async () => {
   try {
     await connectDB(url);
-    app.listen(port, () => {
-      console.log(`listening to port ${port} `);
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-start();
-
-/* const connectDB = require("./db/connect");
-const express = require("express");
-const app = express();
-const port = 8000;
-
-// Define your routes and middleware here
-// For example:
-// app.use(express.json()); // Middleware for parsing JSON
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
-const start = async () => {
-  try {
-    await connectDB();
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
   } catch (err) {
@@ -83,4 +63,3 @@ const start = async () => {
 };
 
 start();
- */
