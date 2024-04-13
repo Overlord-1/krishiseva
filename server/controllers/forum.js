349a77ab62ce469bc9322221f1da3c5f25e8312d
Deep <deeppatel223204@gmail.com>
2024-04-13 13:27:07 +0530

    added new Element model

--- server/controllers/forum.js
+++ server/controllers/forum.js
@@ -1,6 +1,8 @@
 const Question = require("../models/Question");
 const Answer = require("../models/Answer");
-
+const Element = require("../models/element");
+const e = require("express");
+/* 
 const createQuestion = async (req, res) => {
   try {
     const content = {
@@ -122,13 +124,73 @@ const getAll = async (req, res) => {
       .json({ err: err.message || "Internal Server Error" });
   }
 };
+ */
+const createElement = async (req, res) => {
+  try {
+    const content = {
+      user: req.body.user,
+      string: req.body.string,
+      parentElement: req.body.parentElement,
+      likes: req.body.likes,
+      dislikes: req.body.dislikes,
+    };
+    const element = await Element.create(content);
+    return res.status(201).json({ element });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ err: err.message || "Internal Server Error" });
+  }
+};
+const editElement = async (req, res) => {
+  try {
+    const id = req.body.id;
+    const toPerform = req.body.toPerform;
+    const incOrDec = req.body.incOrDec;
+    const element = await Element.findOne({ _id: id });
+    if (!element) {
+      return res.status(404).json({ err: "Question/answer not found" });
+    }
+    if (toPerform === "like") {
+      element.likes += incOrDec === "inc" ? 1 : -1;
+    } else if (toPerform === "dislike") {
+      element.dislikes += incOrDec === "inc" ? 1 : -1;
+    }
+
+    await element.save();
+    return res.status(200).json({ element });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllElements = async (req, res) => {
+  try {
+    let query = {};
+    console.log(req.body);
+    query.parentElement = req.body.parentElement
+      ? req.body.parentElement
+      : null;
+    const element = await Element.find(query);
+    return res.status(200).json({ element });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ err: err.message || "Internal Server Error" });
+  }
+};
 
 module.exports = {
-  createQuestion,
-  getQuestion,
-  edit,
-  createAnswer,
-  getAnswers,
-  getAll,
+  // createQuestion,
+  // getQuestion,
+  // edit,
+  // createAnswer,
+  // getAnswers,
+  // getAll,
+  createElement,
+  editElement,
+  getAllElements,
 };
 // Path: server/models/Question.js
