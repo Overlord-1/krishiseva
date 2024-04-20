7bb4ed27e5f3ab7824ff86fa74d2999d72b8addd
Overlord <ruchirkhare12@gmail.com>
2024-04-20 15:01:14 +0530

    minor ui changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar flex items-center justify-center">
       {/* Sidebar */}
       <Sidebar/>
-      <div className="wrapper text-white" >
+      <div className="wrapper text-white text-3xl" >
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
