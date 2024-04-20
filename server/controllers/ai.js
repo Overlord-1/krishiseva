327f2ebd11eda5354064be1aa3a0e5ec22d5c6db
Deep <deeppatel223204@gmail.com>
2024-04-20 23:00:50 +0530

    predict crop added

--- server/controllers/ai.js
+++ server/controllers/ai.js
@@ -9,6 +9,7 @@ const multerStorage = multer.diskStorage({
     cb(null, `file.${ext}`);
   },
 });
+
 const upload = multer({ storage: multerStorage });
 
 const recognizeDisease = async (req, res) => {
@@ -23,4 +24,57 @@ const recognizeDisease = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
-module.exports = { recognizeDisease, upload };
+const predict = async (req, res) => {
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:5000/predict",
+      req.body
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error predicting:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+const model = require("../services/gemini.js");
+
+const test1 = async (req, res) => {
+  try {
+    const result =
+      await model.generateContent(`Considering a region in ${req.params.region},india; provide the most probable soil conditions in JSON format, including: nitrates, phosphates, potassium, temperature (assuming summer), humidity (assuming monsoon season), pH (acknowledging limitations and typical range for ${req.params.region}'s soil , and rainfall (recognizing it might be a drier than usual year). 
+      donot return anything else.i want numbers only, even they maynot be completely verifed, donot include any comments or other information.your answer should start with{ and end with}. put your result in "", donot use ''or any other symbol., DONOT GIVE N/A`);
+    const ans = JSON.parse(result.response.text());
+
+    //NOTE
+
+    const convertedData = Object.entries(ans).reduce((acc, [key, value]) => {
+      // Convert all values to strings
+      const stringValue = value.toString();
+      // Change the key 'pH' to 'ph'
+      const newKey = key === "pH" ? "ph" : key;
+      // Add the new key-value pair to the accumulator
+      acc[newKey] = stringValue;
+      return acc;
+    }, {});
+
+    console.log(convertedData);
+
+    //NOTE
+
+    const response = await axios.post(
+      "http://127.0.0.1:5000/predict",
+      convertedData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.send(response.data);
+  } catch (error) {
+    // console.error("Error predicting:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+module.exports = { recognizeDisease, upload, predict, test1 };
