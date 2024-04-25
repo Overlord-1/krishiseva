8c97926c8ff8bb04c13994f01b097734f2683867
chahat dholakia <chahatdholakia27@gmail.com>
2024-04-25 22:29:54 +0530

    these changes were made by me

--- client/src/pages/Crop.jsx
+++ client/src/pages/Crop.jsx
@@ -1,10 +1,31 @@
 import Navbar from '@/components/Navbar/Navbar'
 import React from 'react'
 
+
 const Crop = () => {
   return (
-    <div>
-        <Navbar text={"Crop Predictor"} />
+    <div className='min-h-screen bg-kdark h-screen w-full'>
+      <Navbar text={"Crop Predictor"} />
+
+      <div className='w-full h-full flex'>
+
+        <div className="leftside min-w-[50%] bg-black h-full ">
+          <input type="text" placeholder='Enter a place' className='bg-klight px-5 p-2 rounded-full ' />
+
+        </div>
+        <div className="rightside min-w-[50%] bg-red-500 h-full ">
+          <input type="text" placeholder='Enter a place' className='bg-klight px-5 p-2 rounded-full ' />
+
+        </div>
+
+
+      </div>
+
+
+
+
+
+
     </div>
   )
 }
