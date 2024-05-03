09728cecad62736bca4258ffcda96f7a05c23850
chahat dholakia <chahatdholakia27@gmail.com>
2024-05-03 11:25:35 +0530

    ruchir commit

--- client/src/pages/Crop.jsx
+++ client/src/pages/Crop.jsx
@@ -1,33 +1,25 @@
-import Navbar from '@/components/Navbar/Navbar'
-import React from 'react'
-
+import React from 'react';
+import Navbar from '@/components/Navbar/Navbar';
 
 const Crop = () => {
+  const handleSubmit = () => {
+    console.log('Form submitted');
+    // Add your form submission logic here
+  };
+
   return (
     <div className='min-h-screen bg-kdark h-screen w-full'>
       <Navbar text={"Crop Predictor"} />
 
       <div className='w-full h-full flex'>
-
-        <div className="leftside min-w-[50%] bg-black h-full ">
-          <input type="text" placeholder='Enter a place' className='bg-klight px-5 p-2 rounded-full ' />
-
+        <div className="leftside min-w-[50%] mt-10 pl-20">
+          <input type="text" placeholder='Enter a place' className='bg-klight px-16 mr-3 p-2 rounded-full' />
+          <button onClick={handleSubmit} className='bg-klight hover:text-[#0a210f]  text-kdark font-bold py-2 px-4 rounded ml-3'>Submit</button>
         </div>
-        <div className="rightside min-w-[50%] bg-red-500 h-full ">
-          <input type="text" placeholder='Enter a place' className='bg-klight px-5 p-2 rounded-full ' />
-
-        </div>
-
-
       </div>
-
-
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Crop
+export default Crop;
+
