ec4eaec8755b00d530b14776f8c9826cfcd87d90
Ruchir Khare <ruchirkhare12@gmail.com>
2024-05-04 14:52:50 +0530

    Update class to className in ErrorPage and Leaf components

--- client/src/pages/Crop.jsx
+++ client/src/pages/Crop.jsx
@@ -5,8 +5,6 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css'
 import QAComp from '@/components/QAComp';
 
-
-
 const Crop = (props) => {
   const [inputValue, setInputValue] = useState();
   const [loading, setLoading] = useState(false);
@@ -34,41 +32,65 @@ const Crop = (props) => {
       <Navbar text={"Crop Predictor"} />
 
       <div className='w-full flex'>
-        <div className="leftside min-w-[50%] mt-10 pl-20">
-          <input type="text" placeholder='Enter a place' className='bg-klight text-black px-16 mr-3 p-2 rounded-full' onChange={handleInputChange} />
-          <button onClick={handleSubmit} className='bg-klight hover:text-[#0a210f]  text-kdark font-bold py-2 px-4 rounded-lg ml-3'>Search</button>
+        <div className="leftside min-w-[50%] max-w-[50%] mt-10 pl-20">
+
+          <div className='flex flex-col gap-10'>
+            <span className='text-klight'>Enter the name of the place your looking</span>
+            <div>
+              <input type="text" placeholder='Enter a place' className='bg-klight text-black px-16 mr-3 p-2 rounded-full' onChange={handleInputChange} />
+            </div>
+            <span className='text-klight'>Enter the N P K and Ph values of the location</span>
+            <div className='flex flex-col max-w-[300px] gap-10'>
+              <input type="text" placeholder='Enter a place' className='bg-klight text-black mr-3 p-2 rounded-full' onChange={handleInputChange} />
+              <input type="text" placeholder='Enter a place' className='bg-klight text-black mr-3 p-2 rounded-full' onChange={handleInputChange} />
+              <input type="text" placeholder='Enter a place' className='bg-klight text-black mr-3 p-2 rounded-full' onChange={handleInputChange} />
+              <input type="text" placeholder='Enter a place' className='bg-klight text-black mr-3 p-2 rounded-full' onChange={handleInputChange} />
+            </div>
+
+            <button onClick={handleSubmit} className='bg-klight hover:text-[#0a210f]  text-kdark font-bold py-2 px-4 rounded-lg ml-3'>Search</button>
+            <div>
+
+            </div>
+
+            {/* <input type="text" placeholder='Enter a place' className='bg-klight text-black px-16 mr-3 p-2 rounded-full' onChange={handleInputChange} />
+            <button onClick={handleSubmit} className='bg-klight hover:text-[#0a210f]  text-kdark font-bold py-2 px-4 rounded-lg ml-3'>Search</button>
+
+            <input type="text" placeholder='Enter a place' className='bg-klight text-black px-16 mr-3 p-2 rounded-full' onChange={handleInputChange} />
+            <button onClick={handleSubmit} className='bg-klight hover:text-[#0a210f]  text-kdark font-bold py-2 px-4 rounded-lg ml-3'>Search</button> */}
 
 
-          {
-            loading ? <SkeletonTheme baseColor="#0a210f" highlightColor="#e1e28945">
-              <p>
-                <Skeleton className='md:w-[500px] h-[300px] mt-10' />
-              </p>
-              <Skeleton className=' h-3 mt-10' />
-            </SkeletonTheme> : null
-          }
+          </div>
+
+
+
+          
         </div>
 
         <div className="right-side w-full lg:w-1/2 lg:h-full flex flex-col items-center">
           <div className="text-2xl font-bold text-left">Results</div>
-
-          {
-            loading 
-            ? 
-            <QAComp text={"fkdjfkdjkf"} rank={1} disease={true} loading={true}/>
-            :
-            started 
-            ?
-            <QAComp text={"fkdjfkdjkf"} rank={1} disease={true} loading={false} />  
-            :
-            null
-          }
-
-
+          <div> {/* Add this div wrapper */}
+            {
+              loading ? <SkeletonTheme baseColor="#0a210f" highlightColor="#e1e28945">
+                <p>
+                  <Skeleton className='md:w-[500px] h-[300px] mt-10' />
+                </p>
+                <Skeleton className=' h-3 mt-10' />
+              </SkeletonTheme> : null
+            }
+            {
+              loading
+                ?
+                <QAComp text={"fkdjfkdjkf"} rank={1} disease={true} loading={true} />
+                :
+                started
+                  ?
+                  <QAComp text={"fkdjfkdjkf"} rank={1} disease={true} loading={false} />
+                  :
+                  null
+            }
+          </div> {/* Add this div wrapper */}
         </div>
       </div>
-
-
     </div>
   );
 };
